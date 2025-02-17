96baf18f744ff9ee1f996fb6a2de7f9f7b30d2f2
Kishore-RaoK <kishoreraok12@gmail.com>
2025-02-17 15:22:07 +0530

    updated code

--- src/server/6-highest-player-of-the-match-per-season.js
+++ src/server/6-highest-player-of-the-match-per-season.js
@@ -1,50 +1,42 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from "fs";
 
-const matchesData = JSON.parse(readFileSync('./src/data/matches.json'));
-
+const matchesData = JSON.parse(readFileSync("./src/data/matches.json"));
 
 // 6.Find a player who has won the highest number of Player of the Match awards for each seasons
+//get years and player of the match and count
+//compare and return result
 function HighestPlayerOfTheMatchPerSeason() {
-   
-    let playerOfTheMatchEachSeason = {};
-  
-    for (let i = 0; i < matchesData.length; i++) {
-        let season = matchesData[i].season;
-        let player_of_match = matchesData[i].player_of_match;
-  
-      if (!playerOfTheMatchEachSeason[season]) {
-        playerOfTheMatchEachSeason[season] = {};
-      }
-  
-      if (!playerOfTheMatchEachSeason[season][player_of_match]) {
-        playerOfTheMatchEachSeason[season][player_of_match] = 0;
-      }
-  
-      playerOfTheMatchEachSeason[season][player_of_match]++;
+  let result = {};
+  for (let i = 0; i < matchesData.length; i++) {
+    let playerofMathch = matchesData[i].player_of_match;
+    let year = matchesData[i].season;
+    if (!result[year]) {
+      result[year] = {};
     }
-
-    let HighestPlayerOfTheMatchPerSeason = {};
-  
-    for (let season in playerOfTheMatchEachSeason) {
-      let playersInSeason = playerOfTheMatchEachSeason[season];
-      let topPlayer = { player: "", awards: 0 };
-  
-      for (let player in playersInSeason) {
-        const awards = playersInSeason[player];
-        if (awards > topPlayer.awards) {
-          topPlayer = { player, awards };
-        }
-      }
-  
-      HighestPlayerOfTheMatchPerSeason[season] = topPlayer;
+    if (!result[year][playerofMathch]) {
+      result[year][playerofMathch] = 1;
+    } else {
+      result[year][playerofMathch]++;
     }
-  
-    return HighestPlayerOfTheMatchPerSeason;
   }
-  
+  let res = {};
+  for (let year in result) {
+    let maxCount = 0;
+    let maxPlayer = "";
+    for (let player in result[year]) {
+      if (result[year][player] > maxCount) {
+        maxCount = result[year][player];
+        maxPlayer = player;
+      }
+    }
+    res[year] = { Player: maxPlayer, Count: maxCount };
+  }
+  return res;
+}
 
 const result = HighestPlayerOfTheMatchPerSeason();
 
-const outputFile = './src/public/output/6-HighestPlayerOfTheMatchPerSeason.json';
-writeFileSync(outputFile, JSON.stringify(result, null, 2), 'utf-8');
+const outputFile =
+  "./src/public/output/6-HighestPlayerOfTheMatchPerSeason.json";
+writeFileSync(outputFile, JSON.stringify(result, null, 2), "utf-8");
 console.log(`Output has been redirected to public/output dir`);
